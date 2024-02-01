9933047492a667230b661ab17b742b01f522d7fd
Alex L. Deweert <alexander.deweert@gmail.com>
2024-02-01 13:49:56 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <nav>
+      <RouterLink :to="{ name: 'home', query: { bar: 'poop' } }">Home</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+    </nav>
+    <br />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  background-color: blue;
+}
+nav {
+  /* margin: 8px; */
+  background-color: magenta;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const counterStore = useCounterStore()
+
+let bar = route.query.bar
+
+let count = computed(() => counterStore.count)
+
+function handleClicked() {
+  counterStore.increment()
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hello Home - {{ bar }} - {{ count }}</h1>
+    <button @click="handleClicked()">Increment</button>
+  </div>
+</template>
+
+<style></style>
